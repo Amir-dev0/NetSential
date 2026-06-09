bf2bda3831ca2039b793266422ba37ba3c26986e
AMIR <amirmodirizadeh@gmail.com>
2026-06-09 21:59:52 +0330

    Converting the program to oop

--- Agent/log_parser.py
+++ Agent/log_parser.py
@@ -2,42 +2,58 @@
 
 # importing the required modules
 import subprocess
+class LogParse():
+    def __init__(self):
+        pass
+    
+    # This function receives and shows the logs momentarily
+    def realtime_log():
+        process = subprocess.Popen(
+            ["journalctl", "-f"],
+            stdout=subprocess.PIPE,
+            text=True
+        )
 
-def realtime_log():
-    process = subprocess.Popen(
-        ["journalctl", "-f"],
-        stdout=subprocess.PIPE,
-        text=True
-    )
+        for line in process.stdout:
+            print(line.strip())
 
-    for line in process.stdout:
-        print(line.strip())
+    # This function shows error logs
+    def error_logs():
+        process = subprocess.run(
+            ["journalctl", "-p", "err"],
+            capture_output=True,
+            text=True
+        )
 
-def error_log():
-    process = subprocess.run(
-        ["journallctl", ]
+        return process.stdout
 
+    # This function shows the logs from the time you give it until now
+    def time_log():
+        time_input = input("")
+        process = subprocess.run(
+            ["journalctl", "--since", f"{time_input} ago"],
+            capture_output=True,
+            text=True      
+        )
+        return process.stdout
 
-    )
+    # This function shows the errors in real time
+    def live_errors():
+        process = subprocess.Popen(
+            ["journalctl", "-f", "-p", "err"],
+            stdout=subprocess.PIPE,
+            text=True
+            )
 
-def time_log():
-    time_input = input("")
-    process = subprocess.run(
-        ["journalctl", "--since", f"{time_input} ago"],
-        capture_output=True,
-        text=True      
-    )
-    return process.stdout
+        for line in process.stdout:
+            print("ERROR >", line.strip())
 
-def live_errors():
-    process = subprocess.Popen(
-        ["journalctl", "-f", "-p", "err"],
-        stdout=subprocess.PIPE,
-        text=True
-    )
+    # This function shows boot errors
+    def boot_errors():
+        process = subprocess.run(
+            ["journalctl", "-b", "-p", "err"],
+            capture_output=True,
+            text=True
+        )
 
-    for line in process.stdout:
-        print("ERROR >", line.strip())
-
-live_errors()        
-
+        return process.stdout
