5d3df29bb80c7cc77b738827ac72d8883295af7b
AMIR <amirmodirizadeh@gmail.com>
2026-06-14 20:42:08 +0330

    Adding a login feature to the main.py

--- main.py
+++ main.py
@@ -1,43 +1,32 @@
+import getpass
 from Backend.database import Database
 from Agent.port_scanner import PortScanner
 from Agent.system_metrics import SystemMetrics
-
+from Agent.login import LoginAgent
 
 db = Database()
 db.create_table()
+db.create_users_table()
 
-target = input("Enter target: ")
+if LoginAgent.authenticate(db):
+    
+    print("Welcome to NetSential")
+    
+    target = input("Enter target: ")
 
-scanner = PortScanner(target)
+    scanner = PortScanner(target)
 
-mode = int(input(
-    "1) single scan\n"
-    "2) range scan\n"
-    "-> "
-))
+    mode = int(input(
+        "1) single scan\n"
+        "2) range scan\n"
+        "-> "
+    ))
 
-if mode == 1:
+    if mode == 1:
 
-    port = int(input("Port: "))
+        port = int(input("Port: "))
 
-    result = scanner.scan_single_port(port)
-
-    db.save_scan(
-        result["target"],
-        result["port"],
-        result["status"]
-    )
-
-    print(result)
-
-elif mode == 2:
-
-    begin = int(input("Begin port: "))
-    end = int(input("End port: "))
-
-    results = scanner.scan_range(begin, end)
-
-    for result in results:
+        result = scanner.scan_single_port(port)
 
         db.save_scan(
             result["target"],
@@ -45,18 +34,29 @@ elif mode == 2:
             result["status"]
         )
 
-    print(results)
+        print(result)
 
-#db.close()
+    elif mode == 2:
 
-db.create_metric_table()
+        begin = int(input("Begin port: "))
+        end = int(input("End port: "))
 
-metrics_agent = SystemMetrics()
+        results = scanner.scan_range(begin, end)
 
-metrics = metrics_agent.collect_metrics()
+        for result in results:
 
-db.save_system_metrics(metrics)
+            db.save_scan(
+                result["target"],
+                result["port"],
+                result["status"]
+            )
 
-print(metrics)
 
-db.close()
+    db.create_metric_table()
+
+    metrics_agent = SystemMetrics()
+
+    metrics = metrics_agent.collect_metrics()
+
+    db.save_system_metrics(metrics)
+    db.close()
